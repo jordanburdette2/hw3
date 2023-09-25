5ce5b5f8178f9e85a336b08f3b68025cb8c9a731
jordanburdette2 <111701847+jordanburdette2@users.noreply.github.com>
2023-09-25 13:41:06 -0500

    Update model-instructors-with-courses.php

--- model-instructors-with-courses.php
+++ model-instructors-with-courses.php
@@ -16,7 +16,7 @@ function selectInstructors() {
 function selectCoursesByInstructor($iid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT course_id, course_number, course_description, semester, room, day_time FROM `course` c join section s on s.course_id=c.course_id where s.instructor_id=?");
+        $stmt = $conn->prepare("SELECT c.course_id, course_number, course_description, semester, room, day_time FROM `course` c join section s on s.course_id=c.course_id where s.instructor_id=?");
         $stmt->bind_param("i", $iid);  
       $stmt->execute();
         $result = $stmt->get_result();

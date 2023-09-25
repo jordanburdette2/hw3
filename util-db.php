7fe70a0f57f14cb192c8d8d2865544d2d42084a3
jordanburdette2 <111701847+jordanburdette2@users.noreply.github.com>
2023-09-25 13:26:07 -0500

    Update util-db.php

--- util-db.php
+++ util-db.php
@@ -1,7 +1,7 @@
 <?php
 function get_db_connection(){
     // Create connection
-    $conn = new mysqli('159.89.47.44', 'jordanb1_hw3user', mis4013!!, jordanb1_hw3);
+    $conn = new mysqli('159.89.47.44', 'jordanb1_hw3user', 'mis4013!!', jordanb1_hw3);
     
     // Check connection
     if ($conn->connect_error) {

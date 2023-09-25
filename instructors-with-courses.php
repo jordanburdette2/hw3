1e8c5379b216858a1affd3665d4ede32511f363c
jordanburdette2 <111701847+jordanburdette2@users.noreply.github.com>
2023-09-25 13:29:22 -0500

    Update instructors-with-courses.php

--- instructors-with-courses.php
+++ instructors-with-courses.php
@@ -5,6 +5,6 @@ require_once("model-instructors-with-courses.php");
 $pageTitle="Instructors with Courses";
 include "view-header.php";
 $instructors = selectInstructors();
-include "view-instructors-instructors-with-courses.php";
+include "view-instructors-with-courses.php";
 include "view-footer.php";
 ?>
